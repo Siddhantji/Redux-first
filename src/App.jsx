f733399d5aef6e2092675374efdca40bd12493b0
Siddhantji <teotiasiddhant@gmail.com>
2024-09-24 10:48:48 +0530

    added components of container and controls and imported them in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import Container from "./components/Container";
+import Controls from "./components/Controls";
 import DisplayCounter from "./components/DisplayCounter";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div>React+Redux</div>
-      <Header/>
-      <DisplayCounter/>
+      <div className="px-4 py-5 my-5 text-center">
+        <Container>
+      <Header></Header>
+        <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+        <div className="col-lg-6 mx-auto">
+          <DisplayCounter></DisplayCounter>
+          <Controls></Controls>
+        </div>
+        </Container>
+      </div>
     </>
   );
 }
